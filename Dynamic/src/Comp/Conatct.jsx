ecb2b7175aa7e434db8f632bf30102358dba9765
akhil <akhiltuluri123@gmail.com>
2025-08-06 17:02:48 +0530

    Resolved error

--- Dynamic/src/Comp/Conatct.jsx
+++ Dynamic/src/Comp/Conatct.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 
-const conatact = () => {
+const Conatact = () => {
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -102,4 +102,4 @@ const conatact = () => {
   );
 };
 
-export default conatact;
+export default Conatact;
